2f5e51e938e89f2a1aa61cdf0c649be9cdd535df
Andrew Kuznetsov <56806222+endrewsmith@users.noreply.github.com>
2024-12-10 12:43:30 +0300

    Update HomeWork.java

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,8 +3,8 @@ package org.example;
 
 import lombok.SneakyThrows;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.stream.IntStream;
 
 public class HomeWork {
 
@@ -14,9 +14,34 @@ public class HomeWork {
      */
     @SneakyThrows
     public void championship(InputStream in, OutputStream out) {
+        // Создадим красно-черное дерево рыцарей(из вебинара)
+        RedBlackTree<Integer> knights = new RedBlackTree<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        PrintWriter pw = new PrintWriter(out);
 
+        String[] strings = br.readLine().split(" ");
+        int n = Integer.parseInt(strings[0]);
+        int m = Integer.parseInt(strings[1]);
+
+        // Заполним дерево
+        for (int i = 1; i <= n; i++) {
+            knights.add(i);
+        }
+
+        for (int i = 0; i < m; i++) {
+            String[] data = br.readLine().split(" ");
+            int l = Integer.parseInt(data[0]);
+            int r = Integer.parseInt(data[1]);
+            int win = Integer.parseInt(data[2]);
+            knights.setK(win, IntStream.range(l, r + 1).boxed().toArray(Integer[]::new));
+        }
+
+        for (int i = 1; i <= n; i++) {
+            pw.print(knights.getByKeyForWinner(i) == null ? 0 : knights.getByKeyForWinner(i));
+            if (i < n) {
+                pw.print(" ");
+            }
+        }
+        pw.flush();
     }
-
-
-
 }
